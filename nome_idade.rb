c86b92cd2bb9e9c2356f454614b5a67f13e24f71
Renan Magnussão <renanmagnussao@MacBook-Air-M1-de-Renan.local>
2021-05-17 13:12:35 -0300

    Finalizando desafio 2

---
+++ nome_idade.rb
@@ -0,0 +1,7 @@
+print "Qual é o seu nome? "
+nome = gets.chomp
+
+print "Obrigado! E qual é a sua idade? "
+idade = gets.chomp
+
+puts "Obrigado! Então seu nome é #{nome} e sua idade é #{idade} anos."
