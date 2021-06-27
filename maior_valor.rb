012835cb6d09126b4f3094fadf7d3d571979ef40
Renan Magnussão <r.magnussao@icloud.com>
2021-06-27 19:50:11 -0300

    Criando arquivo da terceira missão da aula 4

---
+++ maior_valor.rb
@@ -0,0 +1,4 @@
+# Dado o seguinte hash:
+# Numbers = {a: 10, b: 30 2, c: 20, d: 25, e: 15}
+# Crie uma instrução que seleciona o maior valor deste hash e no final imprima a chave e valor do elemento resultante.
+
