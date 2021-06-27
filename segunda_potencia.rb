8907f36d8fff57d8ab13da5498e45e69cd9a7117
Renan Magnussão <r.magnussao@icloud.com>
2021-06-27 19:00:30 -0300

    Finalizando primeira missão da aula 4

--- segunda_potencia.rb
+++ segunda_potencia.rb
@@ -1,3 +1,22 @@
 #Utilizando uma collection do tipo Array, escreva um programa que receba 3 números e 
 #no final exiba o resultado de cada um deles elevado a segunda potência.
 
+
+numeros = []
+
+puts "Digite o primeiro número: "
+primeiroNumero = gets.chomp.to_i
+numeros.push(primeiroNumero)
+puts "Digite o segundo número: "
+segundoNumero = gets.chomp.to_i
+numeros.push(segundoNumero)
+puts "Digite o terceiro número: "
+terceiroNumero = gets.chomp.to_i
+numeros.push(terceiroNumero)
+
+potenciacao = numeros.map do |potencia|
+    potencia * 2
+end
+
+puts "A seguir os números elevados a segunda potência"
+puts potenciacao
