0aed8a4758d09510a16732f4ad84358506f38ec9
Renan Magnussão <r.magnussao@icloud.com>
2021-06-27 19:47:43 -0300

    Finalizando segunda missão da aula 4

--- chave_valor.rb
+++ chave_valor.rb
@@ -1,2 +1,15 @@
 #Crie uma collection do tipo Hash e permita que o usuário crie três elementos informando a chave e o valor. 
 #No final do programa para cada um desses elementos imprima a frase “Uma das chaves é **** e o seu valor é ****”
+
+hash = Hash.new
+
+3.times do
+print "Digite a chave para seu elemento: "
+chave = gets.chomp
+print "Digite o valor para o seu elemento: "
+valor = gets.chomp
+hash[chave] = valor
+end
+hash.each do |chave, valor|
+puts "Uma das chaves é #{chave} e o seu valor é #{valor}"
+end
