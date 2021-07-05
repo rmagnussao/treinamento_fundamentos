c0e7211a58e57f8148aca1a18d985964661be271
Renan Magnussao <r.magnussao@icloud.com>
2021-07-05 06:03:40 -0300

    Criando arquivo da segunda missão da aula 5

---
+++ cpf_cnpj.rb
@@ -0,0 +1,4 @@
+#Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de cpf e em um método verifique se este número é válido.
+#Link da documntação:
+#https://github.com/fnando/cpf_cnpj
+
