531f6503aa89fcf93c28d5006850b1ffcb2455e9
Renan Magnussão <r.magnussao@icloud.com>
2021-07-17 13:34:07 -0300

    Finalizando segundo desafio da aula 8

--- person.rb
+++ person.rb
@@ -4,3 +4,31 @@
 #Já ao executar a instrução Person::Physical.new('José Almeida', '425.123.123-123').add
 #O código deverá retornar uma mensagem informando que a pessoa física foi adicionada, o nome e o cpf. 
 
+module Person
+    class Juridic
+        def initialize (name, cnpj)
+            @name = name
+            @cnpj = cnpj
+        end
+        def add
+            puts 'Pessoa Jurídica adicionada com sucesso!'
+            puts "Nome: #{@name}"
+            puts "CNPJ: #{@cnpj}"
+        end
+    end
+
+    class Physical
+        def initialize (name, cpf)
+            @name = name
+            @cpf = cpf
+        end
+        def add
+            puts 'Pessoa Física adicionada com sucesso!'
+            puts "Nome: #{@name}"
+            puts "CPF: #{@cpf}"
+        end
+    end
+end
+
+Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+Person::Physical.new('José Almeida', '425.123.123-123').add
