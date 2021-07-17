aa571c1f834c599ab8976e96ccd60593e32f1de6
Renan Magnussão <r.magnussao@icloud.com>
2021-07-17 16:53:58 -0300

    Finalizando segunda missão da aula 9

--- carro.rb
+++ carro.rb
@@ -4,3 +4,19 @@
 #Este deve localizar e retornar o casamento de padrão 80km/h.
 #No final, imprima este retorno. 
 
+class Car
+    def get_km(phrase)
+      km = find_km(phrase)
+      puts km
+    end
+    
+    private
+    
+    def find_km(phrase)
+      /\d{2,}km\/h/.match(phrase)
+    end
+end
+    
+   phrase = "Um fusca de cor amarela viaja a 80km/h"
+   car = Car.new
+   car.get_km(phrase)
