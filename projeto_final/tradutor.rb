fd44149ba2336e8b387037892d9a4ddba6a00b7d
Renan Magnussão <r.magnussao@icloud.com>
2021-07-17 19:49:10 -0300

    Finalizando projeto final do curso de Ruby Puro

--- projeto_final/tradutor.rb
+++ projeto_final/tradutor.rb
@@ -5,3 +5,33 @@
 #O resultado de cada tradução (junto com a frase original) deve ser guardado em um arquivo que possua no nome a data e 
 #horário da tradução (exp: 10-10-18_10:30.txt)
 
+#https://github.com/seejohnrun/easy_translate
+
+require 'easy_translate'
+
+DATE = Time.now
+DATE.strftime('%d/%m/%y')
+
+class Translator
+  def read(currentLanguage, text, newLanguage)
+    @currentLanguage = currentLanguage.to_s
+    @text = text.to_s
+    @newLanguage = newLanguage.to_s
+    generateFile
+  end
+
+  private
+
+  def generateFile
+    File.open(DATE.strftime('%d-%m-%Y_%H:%M:%S.txt') , 'a') do |line|
+      line.puts("Bem vindo ao resultado da aplicação!")
+      line.puts("Abaixo algumas informações sobre a execução: ")
+      line.puts("\n\nTexto inserido para a tradução: #{@text}")
+      line.puts("Idioma do texto: #{@currentLanguage.upcase}")
+      line.puts("\n\nTexto após a tradução: #{EasyTranslate.translate(@text, from: @currentLanguage, to: @newLanguage,
+        key: 'AIzaSyCx-2gLqcXeoR7a9V9J1W8b5ZenmIIz36o')}")
+      line.puts("Idioma escolhido para a tradução: #{@newLanguage.upcase}")
+    end
+    print 'Tradução concluída com sucesso! Verifique o arquivo para ver o resultado! =)'
+  end
+end
