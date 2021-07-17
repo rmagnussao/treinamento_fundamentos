09bbaa168b1fd1115ee7767bb84dcbf3280cd3e9
Renan Magnussão <r.magnussao@icloud.com>
2021-07-17 16:23:52 -0300

    Criando segunda missão da aula 9

---
+++ carro.rb
@@ -0,0 +1,6 @@
+#Em uma classe chamada Carro, crie um método público chamado get_km que recebe como parâmetro a seguinte informação 
+#“Um fusca de cor amarela viaja a 80km/h ”.
+#O método get_km deve chamar um método privado com o nome de find_km. 
+#Este deve localizar e retornar o casamento de padrão 80km/h.
+#No final, imprima este retorno. 
+
