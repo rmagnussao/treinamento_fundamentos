5559d614c7f14842fac55baa636f483151cc341f
Renan Magnussão <r.magnussao@icloud.com>
2021-07-17 12:23:15 -0300

    Criando os arquivos das missões um e dois da aula 8

---
+++ capitalize.rb
@@ -0,0 +1,5 @@
+#O ruby oferece um método chamado capitalize para tornar a primeira letra de uma string maiúscula.
+#Sabendo disso crie uma lambda que recebe um nome como parâmetro e o imprime com a primeira letra maiúscula. 
+#Esta lambda deverá ser salva dentro de uma variável que será passada como argumento de um método chamado capitalize_name.
+#Dentro deste método você chamará a lambda duas vezes, passando como parâmetro em cada uma delas um nome diferente. 
+
---
+++ person.rb
@@ -0,0 +1,6 @@
+#Crie um módulo chamado Person com as classes Juridic e Physical.
+#Ao executar a instrução Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+#O código deverá retornar uma mensagem informando que a pessoa jurídica foi adicionada, o nome e o cnpj. 
+#Já ao executar a instrução Person::Physical.new('José Almeida', '425.123.123-123').add
+#O código deverá retornar uma mensagem informando que a pessoa física foi adicionada, o nome e o cpf. 
+
